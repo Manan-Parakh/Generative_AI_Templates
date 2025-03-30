7d40436f6b12da425916f2bec1be3769bff15e98
Manan Parakh <mananparakh500@gmail.com>
2025-03-30 20:31:51 +0530

    Added Ollama as an option

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import openai
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
 import os
 
 import os
@@ -24,35 +25,40 @@ prompt=ChatPromptTemplate.from_messages(
 
 def generate_response(question,api_key,llm,temperature,max_tokens):
     
-    llm = ChatOpenAI(model=llm, openai_api_key=api_key)
+    if (llm == 'gemma3:1b'):
+        llm = Ollama(model=llm)
+    else:
+        llm = ChatOpenAI(model=llm, openai_api_key=api_key)
     output_parser=StrOutputParser()
     chain=prompt|llm|output_parser
     answer=chain.invoke({'question':question})
     return answer
 
 ## Title of the app
-st.title("Enhanced Q&A Chatbot With OpenAI")
+st.title("Enhanced Q&A Chatbot With OpenAI/Ollama")
+## Select the model provider first
+model_provider = st.sidebar.selectbox("Select Model Provider", ["OpenAI", "Ollama"])
 
-## Sidebar for settings
-st.sidebar.title("Settings")
-api_key=st.sidebar.text_input("Enter your OpenAI API Key:",type="password")
-
-## Select the OpenAI model
-engine=st.sidebar.selectbox("Select OpenAI model",["gpt-4","gpt-4-turbo","gpt-4"])
+if model_provider == "OpenAI":
+    api_key = st.sidebar.text_input("Enter your OpenAI API Key:", type="password")
+    engine = st.sidebar.selectbox("Select OpenAI model", ["gpt-4", "gpt-4-turbo", "gpt-4"])
+else:
+    api_key = None  # No API key needed for Ollama
+    engine = st.sidebar.selectbox("Select Ollama model", ["gemma3:1b"])
 
 ## Adjust response parameter
-temperature=st.sidebar.slider("Temperature",min_value=0.0,max_value=1.0,value=0.7)
+temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
 max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
 
 ## Main interface for user input
 st.write("Go ahead and ask any question")
-user_input=st.text_input("You:")
+user_input = st.text_input("You:")
 
-if user_input and api_key:
-    response=generate_response(user_input,api_key,engine,temperature,max_tokens)
-    st.write(response)
-
-elif user_input:
-    st.warning("Please enter the OpenAI API Key in the sidebar")
+if user_input:
+    if model_provider == "OpenAI" and not api_key:
+        st.warning("Please enter the OpenAI API Key in the sidebar")
+    else:
+        response = generate_response(user_input, api_key, engine, temperature, max_tokens)
+        st.write(response)
 else:
     st.write("Please provide the user input")
