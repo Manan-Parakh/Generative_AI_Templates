fa57e7a74f3cc8c16f69f694b23fd7f4f6a2e29e
Manan Parakh <mananparakh500@gmail.com>
2025-05-13 01:12:23 +0530

    Created the sqlite3 database

---
+++ Chat_SQL/sqlite.py
@@ -0,0 +1,33 @@
+import sqlite3
+
+# Connect to sqlite3
+connection = sqlite3.Connection('student.db')
+
+# Create a Cursor to create table and enter data
+cursor = connection.cursor()
+
+# Create the Table
+table_info = """
+create table STUDENT(NAME VARCHAR(25), CLASS VARCHAR(25), SECTION VARCHAR(25), MARKS INT)
+"""
+
+cursor.execute(table_info)
+
+# Insert some records
+cursor.execute("""Insert into STUDENT values('Krish',"Data Science","A",90)""")
+cursor.execute("""Insert into STUDENT values('John','Data Science','B',100)""")
+cursor.execute("""Insert into STUDENT values('Mukesh','Data Science','A',86)""")
+cursor.execute("""Insert into STUDENT values('Jacob','Devops','A',50)""")
+cursor.execute("""Insert into STUDENT values('Dipesh','Devops','A',35)""")
+
+# Display all the records
+print("The Inserted Records are:")
+data = cursor.execute('SELECT * from STUDENT')
+
+for row in data:
+    print(row)
+
+# Commit the changes to the database
+connection.commit()
+connection.close()
+
