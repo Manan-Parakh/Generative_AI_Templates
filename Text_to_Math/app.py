6a50b67ab67a0069f2bb1e4e9773ec1fc165d360
Manan Parakh <mananparakh500@gmail.com>
2025-05-17 02:05:53 +0530

    debug.

--- Text_to_Math/app.py
+++ Text_to_Math/app.py
@@ -18,6 +18,12 @@ load_dotenv()
 st.set_page_config(page_title="Text to Math Problem Solver and Data Search Assistant", page_icon="🧮")
 st.title('Text to Math Problem Solver')
 
+# LangChain tracing env vars (for tracing LLM chains)
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGSMITH_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGCHAIN_PROJECT"] = "Text_to_Math"
+
 # setup the llm
 groq_api_key = st.sidebar.text_input('Groq API Key', type='password')
 if not groq_api_key:
